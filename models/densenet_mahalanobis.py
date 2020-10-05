b59786ba51c6a13b01f558447d5f053946f7a0ab
matanhs <matan.hrs@gmail.com>
2020-10-05 16:16:10 +0300

    add update mahalanobis with identity modules to track conv outputs explicitly

--- models/densenet_mahalanobis.py
+++ models/densenet_mahalanobis.py
@@ -27,13 +27,14 @@ class BottleneckBlock(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.conv1 = nn.Conv2d(in_planes, inter_planes, kernel_size=1, stride=1,
                                padding=0, bias=False)
+        self.conv1_out = nn.Identity()
         self.bn2 = nn.BatchNorm2d(inter_planes)
         self.conv2 = nn.Conv2d(inter_planes, out_planes, kernel_size=3, stride=1,
                                padding=1, bias=False)
         self.conv2_out = nn.Identity()
         self.droprate = dropRate
     def forward(self, x):
-        out = self.conv1(self.relu(self.bn1(x)))
+        out = self.conv1_out(self.conv1(self.relu(self.bn1(x))))
         if self.droprate > 0:
             out = F.dropout(out, p=self.droprate, inplace=False, training=self.training)
         out = self.conv2(self.relu(self.bn2(out)))
@@ -49,14 +50,14 @@ class TransitionBlock(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1,
                                padding=0, bias=False)
+        self.conv1_out = nn.Identity()
         self.droprate = dropRate
-        self.avg_pool = nn.AvgPool2d(2)
-
     def forward(self, x):
         out = self.conv1(self.relu(self.bn1(x)))
+        out = self.conv1_out(out)
         if self.droprate > 0:
             out = F.dropout(out, p=self.droprate, inplace=False, training=self.training)
-        return self.avg_pool(out)
+        return F.avg_pool2d(out,2)
 
 class DenseBlock(nn.Module):
     def __init__(self, nb_layers, in_planes, growth_rate, block, dropRate=0.0):
@@ -84,6 +85,7 @@ class DenseNet3(nn.Module):
         # 1st conv before any dense block
         self.conv1 = nn.Conv2d(3, in_planes, kernel_size=3, stride=1,
                                padding=1, bias=False)
+        self.conv1_out = nn.Identity()
         # 1st block
         self.block1 = DenseBlock(n, in_planes, growth_rate, block, dropRate)
         in_planes = int(in_planes+n*growth_rate)
@@ -115,7 +117,7 @@ class DenseNet3(nn.Module):
                 m.bias.data.zero_()
                     
     def forward(self, x):
-        out = self.conv1(x)
+        out = self.conv1_out(self.conv1(x))
         out = self.trans1(self.block1(out))
         out = self.trans2(self.block2(out))
         out = self.block3(out)
